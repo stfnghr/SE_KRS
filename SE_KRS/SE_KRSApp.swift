a5d32de668eb2d7a44401dbc6dafc75aaee6a83a
Stefanie Agahari <stefanie.agahari@gmail.com>
2025-05-24 22:22:36 +0700

    Initial Commit

---
+++ SE_KRS/SE_KRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SE_KRSApp.swift
+//  SE_KRS
+//
+//  Created by Stefanie Agahari on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SE_KRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
